e3af64fd57a845e44ca5d417597a9573862b3eaf
brunonguyen-del <bruno.nguyen@kobercovyservis.cz>
2026-02-09 23:29:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,11 +7,13 @@ window.APP_CONFIG = {
   GOOGLE_MAPS_API_KEY: "AIzaSyCLw_AH4X1T0YisInKWoBcHcVNkivDrqdY",
 
   // Data pro sběrná místa (CSV/JSON url) – zatím placeholder
-  PLACES_DATA_URL: "./assets/places.csv",
+  PLACES_DATA_URL: "https://docs.google.com/spreadsheets/d/1CJYobC4NWdUv03alqpUYyte--Fw-tiRilqZuYzE-87g/export?format=csv",
+",
 
   // Ceník
   PRICE_CLEAN_PER_M2: 300,
   PRICE_EDGE_PER_M: 99,
   PRICE_IMP_PER_M2: 40
 };
+
 
